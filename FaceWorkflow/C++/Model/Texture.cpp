68d0413836ce62dfeeb8af63d0bfa394fcff4ade
lrj <lirenjun@camera360.com>
2021-02-03 11:34:38 +0800

    更新Texture接口

--- FaceWorkflow/C++/Model/Texture.cpp
+++ FaceWorkflow/C++/Model/Texture.cpp
@@ -35,35 +35,189 @@ Texture_::Texture_(int width, int height, bgfx::TextureFormat::Enum formmat, uin
     m_texure = bgfx::createTexture2D(width, height, false, 1, formmat, _flags, mem);
 }
 
-Texture:: Texture() {
+typedef struct {
+    float x;
+    float y;
+    float z;
+    
+    float u;
+    float v;
+} vertex_struct;
+
+static vertex_struct simple_vertex_data[] =
+{
+    {-1.0f, 1.0f, 0.0f, 0, 0},
+    {1.0f, 1.0f, 0.0f, 1, 0},
+    {-1.0f, -1.0f, 0.0f, 0, 1},
+    {1.0f, -1.0f, 0.0f, 1, 1},
+};
+
+static uint16_t simple_triangle_list[] =
+{
+    0, 2, 1,
+    1, 2, 3
+};
+
+Texture::Texture() {
     
 }
 
-Texture:: ~Texture() {
+Texture::~Texture() {
     
 }
 
+Texture::Texture(const Texture &tex) {
+    
+}
+
+Texture::Texture(Texture &input, unsigned int x, unsigned int y, unsigned int width, unsigned
+                 int height) {
+    
+}
+
+Texture::Texture(int width, int height, const bgfx::Memory *mem, bgfx::TextureFormat::Enum format, uint64_t _flags) {
+    
+}
+
+Texture::Texture(int width, int height, bgfx::TextureFormat::Enum eFormat, uint64_t _flags) {
+    
+}
+
+// PGMat 还没有引入
+//    Texture(const PGMat &m,uint64_t _flags = BGFX_TEXTURE_RT| BGFX_SAMPLER_U_CLAMP | BGFX_SAMPLER_V_CLAMP);
+Texture::Texture &operator = (const unsigned char value) {
+    
+}
+
+//    Texture &operator = (const PGMat &m);
+
+long Texture::getSpCount() {
+    return 1;
+}
+
+unsigned long long Texture::getPtr() {
+    return 1;
+}
+int Texture::channels() const {
+    return 1;
+}
+int Texture::type() const {
+    
+}
+bool Texture::empty() const {
+    
+}
 
 bgfx::TextureHandle Texture::getTextureHandle() {
-    return BGFX_INVALID_HANDLE;
+    
 }
-
 int Texture::getWidth() const {
-    return 0;
+    
+}
+int Texture::getHeight() const{
+    
+}
+uint64_t Texture::getFlags() const {
+    
+}
+uint32_t Texture::getAppFlags() const {
+    
+}
+bool Texture::immutable() const {
+    
+}
+bgfx::TextureFormat::Enum Texture::getTextureFormat() const {
+    
+}
+bool Texture::isValid() {
+    
 }
 
-int Texture::getHeight() const {
-    return 0;
+#pragma mark - 接口函数
+#pragma mark 填充纹理数据接口
+bool Texture::create(const char *name) {
+    
 }
 
+bool Texture::create(int width, int height,
+            bgfx::TextureFormat::Enum eFormat = bgfx::TextureFormat::Enum::RGBA8,
+                     uint64_t _flags = BGFX_TEXTURE_RT | BGFX_SAMPLER_U_CLAMP | BGFX_SAMPLER_V_CLAMP) {
+    
+}
+
+bool Texture::create(int width, int height, const bgfx::Memory *mem,
+            bgfx::TextureFormat::Enum eFormat = bgfx::TextureFormat::Enum::RGBA8,
+                     uint64_t _flags = BGFX_TEXTURE_RT | BGFX_SAMPLER_U_CLAMP | BGFX_SAMPLER_V_CLAMP) {
+    
+}
+
+bool Texture::create(int width, int height,unsigned char r,unsigned char g,unsigned char b,unsigned char a,
+            bgfx::TextureFormat::Enum eFormat = bgfx::TextureFormat::Enum::RGBA8,
+                     uint64_t _flags = BGFX_TEXTURE_RT | BGFX_SAMPLER_U_CLAMP | BGFX_SAMPLER_V_CLAMP) {
+    
+}
+
+#pragma mark 其他接口
+void Texture::release() {
+    
+}
+
+Texture Texture::clone() {
+    
+}
+
+void Texture::update(int dstx, int dsty, Texture &src, int srcx = 0, int srcy = 0, int width = 0, int height = 0) {
+    
+}
+
+//    void update(unsigned int dstx. unsigned int dsty, PGMat &src);
+void Texture::setTo(const unsigned char r, const unsigned char g, const unsigned char b, const unsigned char a) {
+    
+}
+
+void Texture::setTo(int component_idx, const unsigned char value) {
+    
+}
+
+//    PGMat toCpu(PGTEXTURE_CVT_PGMAT_FLAG flag = PGTEXTURE_CVT_PGMAT_FLAG::NOFLIP,bool bBlocking = true);
+//
+//    void operator >>(PGMat &m);
 bool Texture::createFrameBuffer() {
-    return  false;
+    
 }
 
-bgfx::FrameBufferHandle Texture::getFrameBufferHandle() {
-    return BGFX_INVALID_HANDLE;
+bool Texture::createFrameBuffer(Texture &depth) {
+    
 }
 
 bool Texture::releaseFrameBuffer() {
-    return false;
+    
+}
+
+bgfx::FrameBufferHandle Texture::getFrameBufferHandle() {
+    
+}
+
+void Texture::trans2gray(Texture &output) {
+    
+}
+
+// 倒置
+void Texture::invert(Texture &output) {
+    
+}
+
+void Texture::getSingleChannel(Texture &output, int channelnum = 1) {
+    
+}
+
+void Texture::gtSingelChannelInvert(Texture &output,int channelnum = 1) {
+    
+}
+
+void Texture::edgeblack() {
+    
+}
+void Texture::getTextureData(int viewId, unsigned char *data) {
+    
 }
--- FaceWorkflow/C++/Model/Texture.hpp
+++ FaceWorkflow/C++/Model/Texture.hpp
@@ -7,7 +7,7 @@
 
 #ifndef Texture_hpp
 #define Texture_hpp
-
+#include <memory>
 #include <stdio.h>
 #include <bgfx/bgfx.h>
 
@@ -35,17 +35,73 @@ public:
 };
 
 class Texture {
+private:
+    std::shared_ptr<Texture_> m_bgfxtTexture = nullptr;
 public:
     Texture();
     ~Texture();
     
-    bgfx::TextureHandle getTextureHandle();
+    Texture(const Texture &tex);
+    Texture(Texture &input, unsigned int x, unsigned int y, unsigned int width, unsigned
+            int height);
+    Texture(int width, int height, const bgfx::Memory *mem, bgfx::TextureFormat::Enum format = bgfx::TextureFormat::Enum::RGBA8, uint64_t _flags =  BGFX_TEXTURE_RT | BGFX_SAMPLER_U_CLAMP | BGFX_SAMPLER_V_CLAMP);
+    Texture(int width, int height,
+              bgfx::TextureFormat::Enum eFormat = bgfx::TextureFormat::Enum::RGBA8,
+              uint64_t _flags =  BGFX_TEXTURE_RT | BGFX_SAMPLER_U_CLAMP | BGFX_SAMPLER_V_CLAMP);
+    // PGMat 还没有引入
+//    Texture(const PGMat &m,uint64_t _flags = BGFX_TEXTURE_RT| BGFX_SAMPLER_U_CLAMP | BGFX_SAMPLER_V_CLAMP);
+    Texture &operator = (const unsigned char value);
     
+//    Texture &operator = (const PGMat &m);
+    
+    long getSpCount();
+    unsigned long long getPtr();
+    int channels() const;
+    int type() const;
+    bool empty() const;
+    bgfx::TextureHandle getTextureHandle();
     int getWidth() const;
     int getHeight() const;
+    uint64_t getFlags() const;
+    uint32_t getAppFlags() const;
+    bool immutable() const;
+    bgfx::TextureFormat::Enum getTextureFormat() const;
+    bool isValid();
+    
+#pragma mark - 接口函数
+#pragma mark 填充纹理数据接口
+    bool create(const char *name);
+    
+    bool create(int width, int height,
+                bgfx::TextureFormat::Enum eFormat = bgfx::TextureFormat::Enum::RGBA8,
+                uint64_t _flags = BGFX_TEXTURE_RT | BGFX_SAMPLER_U_CLAMP | BGFX_SAMPLER_V_CLAMP);
+    bool create(int width, int height, const bgfx::Memory *mem,
+                bgfx::TextureFormat::Enum eFormat = bgfx::TextureFormat::Enum::RGBA8,
+                uint64_t _flags = BGFX_TEXTURE_RT | BGFX_SAMPLER_U_CLAMP | BGFX_SAMPLER_V_CLAMP);
+    bool create(int width, int height,unsigned char r,unsigned char g,unsigned char b,unsigned char a,
+                bgfx::TextureFormat::Enum eFormat = bgfx::TextureFormat::Enum::RGBA8,
+                uint64_t _flags = BGFX_TEXTURE_RT | BGFX_SAMPLER_U_CLAMP | BGFX_SAMPLER_V_CLAMP);
+#pragma mark 其他接口
+    void release();
+    Texture clone();
+    void update(int dstx, int dsty, Texture &src, int srcx = 0, int srcy = 0, int width = 0, int height = 0);
+//    void update(unsigned int dstx. unsigned int dsty, PGMat &src);
+    void setTo(const unsigned char r, const unsigned char g, const unsigned char b, const unsigned char a);
+    void setTo(int component_idx, const unsigned char value);
+//    PGMat toCpu(PGTEXTURE_CVT_PGMAT_FLAG flag = PGTEXTURE_CVT_PGMAT_FLAG::NOFLIP,bool bBlocking = true);
+//
+//    void operator >>(PGMat &m);
     bool createFrameBuffer();
-    bgfx::FrameBufferHandle getFrameBufferHandle();
+    bool createFrameBuffer(Texture &depth);
     bool releaseFrameBuffer();
+    bgfx::FrameBufferHandle getFrameBufferHandle();
+    void trans2gray(Texture &output);
+    // 倒置
+    void invert(Texture &output);
+    void getSingleChannel(Texture &output, int channelnum = 1);
+    void gtSingelChannelInvert(Texture &output,int channelnum = 1);
+    void edgeblack();
+    void getTextureData(int viewId, unsigned char *data);
 };
 
 #endif /* Texture_hpp */
