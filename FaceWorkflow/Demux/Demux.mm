e787aff4ac78ff7fca6f3eb77b81a8287ee4b143
lrj <lirenjun@camera360.com>
2021-07-23 10:50:16 +0800

    update

--- FaceWorkflow/Demux/Demux.mm
+++ FaceWorkflow/Demux/Demux.mm
@@ -42,11 +42,14 @@ return ret; \
     int _vStreamIdx;
     NSFileHandle *aOutFile;
     NSFileHandle *vOutFile;
+    int _sampleSize;
+    int _sampleFrameSize;
+    uint8_t *_imgBuf[4];
+    int _imageLinesizes[4];
+    int _imgSize;
 }
 @property (nonatomic, strong) AudioDecodeSpec *aOut;
 @property (nonatomic, strong) VideoDecodeSpec *vOut;
-
-
 @end
 
 @implementation Demux
@@ -57,11 +60,7 @@ return ret; \
     AVPacket *packet = nullptr;
     int ret = 0;
     __weak typeof(self) weakSelf = self;
-    [[NSFileManager defaultManager]createFileAtPath:self.aOut.filename contents:nil attributes:nil];
-    [[NSFileManager defaultManager]createFileAtPath:self.vOut.filename contents:nil attributes:nil];
-    aOutFile = [NSFileHandle fileHandleForWritingAtPath:self.aOut.filename];
-    vOutFile = [NSFileHandle fileHandleForWritingAtPath:self.vOut.filename];
-    
+
     // 创建解封装上下文
     ret = avformat_open_input(&_fmtCtx, infileName.UTF8String, nullptr, nullptr);
     END(avformat_open_input);
@@ -132,22 +131,72 @@ end:
 
 // 初始化音频信息
 - (int)initAudionInfo {
-    return 1;
+    int ret = [self initDecoder:&_aDecodeCtx streamIdx:&_aStreamIdx mediaType:AVMEDIA_TYPE_AUDIO];
+    RET(initDecoder);
+    [[NSFileManager defaultManager]createFileAtPath:self.aOut.filename contents:nil attributes:nil];
+    aOutFile = [NSFileHandle fileHandleForWritingAtPath:self.aOut.filename];
+    // 保存音频参数
+    _aOut.sampleRate = _aDecodeCtx->sample_rate;
+    _aOut.sampleFmt = _aDecodeCtx->sample_fmt;
+    _aOut.chLayout = (int)_aDecodeCtx->channel_layout;
+    // 音频样本帧的大小
+    _sampleSize = av_get_bytes_per_sample((AVSampleFormat)_aOut.sampleFmt);
+    _sampleFrameSize = _sampleSize * _aDecodeCtx->channels;
+    return 0;
 }
-
 
 // 初始化视频信息
 - (int)initVideoInfo {
-    return 1;
+    int ret = [self initDecoder:&_aDecodeCtx streamIdx:&_aStreamIdx mediaType:AVMEDIA_TYPE_VIDEO];
+    RET(initDecoder);
+    [[NSFileManager defaultManager]createFileAtPath:self.vOut.filename contents:nil attributes:nil];
+    vOutFile = [NSFileHandle fileHandleForWritingAtPath:self.vOut.filename];
+    if (vOutFile) {
+        return -1;
+    }
+    // 保存视频参数
+    _vOut.width = _vDecodeCtx->width;
+    _vOut.height = _vDecodeCtx->height;
+    _vOut.pixFmt = _vDecodeCtx->pix_fmt;
+    // 帧率
+    //    AVRational frameRate = av_guess_frame_rate(_fmtCtx, _fmtCtx->streams[_vStreamIdx], nullptr);
+    _vOut.fps = _vDecodeCtx->framerate.num;
+    // 创建用于存放一帧解码图片的缓冲区
+    ret = av_image_alloc(_imgBuf, _imageLinesizes, _vOut.width , _vOut.height, (AVPixelFormat)_vOut.pixFmt, 1);
+    RET(av_image_alloc);
+    _imgSize = ret;
+    return 0;
 }
 
 // 初始化解码器
 - (int)initDecoder:(AVCodecContext**)decodeCtx streamIdx:(int *)streamIdx mediaType:(AVMediaType)type {
+    // 根军Type寻找最合适的流信息
+    // 返回值是流索引
+    int ret = av_find_best_stream(_fmtCtx, type, -1, -1, nullptr, 0);
+    RET(av_find_best_stream);
+    // 检验流
+    *streamIdx = ret;
+    AVStream *stream = _fmtCtx->streams[*streamIdx];
+    if (!stream) {
+        return -1;
+    }
+    // 为当前流找到合适的解码器
+    AVCodec *decoder = avcodec_find_decoder(stream->codecpar->codec_id);
+    if (!decoder) {
+        return -1;
+    }
+    // 初始化解码器上下文
+    *decodeCtx = avcodec_alloc_context3(decoder);
+    if (!decodeCtx) {
+        return -1;
+    }
+    // 从流中拷贝参数到解码器上下文中
+    ret = avcodec_parameters_to_context(*decodeCtx, stream->codecpar);
+    RET(avcodec_parameters_to_context);
+    // 打开解码器
+    ret = avcodec_open2(*decodeCtx, decoder, nullptr);
+    RET(avcodec_open2);
     return 0;
-}
-
-int test(int a) {
-    return 1;
 }
 
 // 解码
@@ -168,13 +217,34 @@ int test(int a) {
     return 1;
 }
 
-
 - (void)writeVideoFrame {
+    av_image_copy(_imgBuf,
+                  _imageLinesizes,
+                  (const uint8_t**)_frame->data,
+                  _frame->linesize,
+                  (AVPixelFormat)_vOut.pixFmt,
+                  _vOut.width,
+                  _vOut.height);
+    // 将缓冲区的数据写入文件
+    [vOutFile writeData:[NSData dataWithBytes:_imgBuf[0] length:_imgSize]];
     
 }
 
 - (void)writeAudioFrame {
-    
+    // libfdk_aac解码器，解码出来的PCM格式：s16
+    // aac解码器，解码出来的PCM格式：ftlp
+    if (av_sample_fmt_is_planar((AVSampleFormat)_aOut.sampleFmt)) {
+        // 外层循环：每一个声道的样本数
+        for (int si = 0; si < _frame->nb_samples; si++) {
+            // 内层循环：有多少个声道
+            for (int ci = 0; ci < _aDecodeCtx->channels; ci++) {
+                char *begin = (char*)(_frame->data[ci] + si * _sampleSize);
+                [aOutFile writeData:[NSData dataWithBytes:begin length:_sampleSize]];
+            }
+        }
+    } else {
+        [aOutFile writeData:[NSData dataWithBytes:_frame->data[0] length:_frame->nb_samples * _sampleFrameSize]];
+    }
 }
 
 @end
