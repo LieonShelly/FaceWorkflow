b28b75e5c0d4a46246dc566d9d9d9af59bbb0d85
lrj <lirenjun@camera360.com>
2021-09-08 11:13:17 +0800

    update

--- Algri/main.swift
+++ Algri/main.swift
@@ -168,5 +168,35 @@ class Solution {
         }
         return height
     }
+    
+    
+    /**
+     给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+
+     有效 二叉搜索树定义如下：
+
+     节点的左子树只包含 小于 当前节点的数。
+     节点的右子树只包含 大于 当前节点的数。
+     所有左子树和右子树自身必须也是二叉搜索树
+     
+     
+     - 采用中序遍历：左节点 根节点 右节点 ,中序遍历的作用是排序，如果是搜索二叉树的话，中序遍历的结果是一个升序
+     - 所以要如果中序遍历的结果不是升序，那么就不是搜索二叉树
+     */
+    var pre: Int = .min
+    func isValidBST(_ root: TreeNode?) -> Bool {
+        guard let root = root else {
+            return false
+        }
+        if !isValidBST(root.left) {
+            return false
+        }
+        if root.val <= pre {
+            return false
+        }
+        pre = root.val
+        return isValidBST(root.right)
+    }
+    
 }
 
