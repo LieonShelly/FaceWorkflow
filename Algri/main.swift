eb0c88e45fe057ba2792ab21695ff534ec4c9ceb
lrj <lirenjun@camera360.com>
2021-09-10 09:23:37 +0800

    update

--- Algri/main.swift
+++ Algri/main.swift
@@ -115,11 +115,6 @@ class Solution3 {
         return leftIndex + 1
     }
 }
-var array = [0,0,1,1,1,2,2,3,3,4]
-let count =  Solution3.removeDuplicates(&array)
-print(count)
-print(array)
-
 // 二叉树的最大深度
 /**
  - 采用层序遍历
@@ -186,7 +181,7 @@ class Solution {
     var pre: Int = .min
     func isValidBST(_ root: TreeNode?) -> Bool {
         guard let root = root else {
-            return false
+            return true
         }
         if !isValidBST(root.left) {
             return false
@@ -198,5 +193,214 @@ class Solution {
         return isValidBST(root.right)
     }
     
+    /**
+     给定一个二叉树，检查它是否是镜像对称的。
+     - 判断二叉树是否镜像
+        - 左右先入队
+        - 队列每次出队两个left right， 比较大小
+        - left.left, right.right 入队； left.right, right.left入队
+
+     */
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        guard let root = root else {
+            return true
+        }
+        var queue: [TreeNode?] = []
+        queue.append(root.left)
+        queue.append(root.right)
+        while !queue.isEmpty {
+            let left = queue.removeFirst()
+            let right = queue.removeFirst()
+            if left == nil && right == nil {
+                continue
+            }
+            if left == nil || right == nil {
+                return false
+            }
+            if left?.val != right?.val {
+                return false
+            }
+            queue.append(left?.left)
+            queue.append(right?.right)
+            queue.append(left?.right)
+            queue.append(right?.left)
+        }
+        return true
+    }
+    
+    // 层序遍历
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let root = root else {
+            return []
+        }
+        var queue: [TreeNode] = []
+        queue.append(root)
+        var levelSize = 1
+        var levelVal: [Int] = []
+        var all: [[Int]] = []
+        while !queue.isEmpty {
+            let head = queue.removeFirst()
+            levelVal.append(head.val)
+            levelSize -= 1
+            if head.left != nil {
+                queue.append(head.left!)
+            }
+            if head.right != nil {
+                queue.append(head.right!)
+            }
+            if levelSize == 0 { // 一层结束
+                levelSize = queue.count
+                all.append(levelVal)
+                levelVal.removeAll()
+            }
+        }
+        return all
+    }
+    
+    // 采用二分查找，中序遍历，递归的方式实现
+    func sortedArrayToBST(_ nums: [Int]) -> TreeNode? {
+        guard !nums.isEmpty else {
+            return nil
+        }
+        return createBSTNode(nums.sorted(), startIdx: 0, endIdx: nums.count - 1)
+    }
+    
+    func createBSTNode(_ nums: [Int], startIdx: Int, endIdx: Int) -> TreeNode? {
+        if startIdx > endIdx {
+            return nil
+        }
+        let mid = (startIdx + endIdx) >> 1
+        let root = TreeNode(nums[mid])
+        root.left = createBSTNode(nums, startIdx: startIdx, endIdx: mid - 1)
+        root.right = createBSTNode(nums, startIdx: mid + 1, endIdx: endIdx)
+        return root
+    }
+    
+    // 反转字符串
+    // 输入：["h","e","l","l","o"]
+    // 输出：["o","l","l","e","h"]
+    /*
+    输入：
+    ["A"," ","m","a","n",","," ","a"," ","p","l","a","n",","," ","a"," ","c","a","n","a","l",":"," ","P","a","n","a","m","a"]
+    输出：
+    ["a","m","a","n","a","P"," ",":","l","a","n","a","c"," "," ","a",",","n","a","l","p"," ","a"," ",",","n","a","m"," ","A"]
+    预期结果：
+    ["a","m","a","n","a","P"," ",":","l","a","n","a","c"," ","a"," ",",","n","a","l","p"," ","a"," ",",","n","a","m"," ","A"]
+ */
+    func reverseString(_ s: inout [Character]) {
+        let endIndx = (s.count - 1) / 2
+        for index in 0 ... endIndx {
+            let temp = s [s.count - 1 - index]
+            s[s.count - 1 - index] = s[index]
+            s[index] = temp
+        }
+    }
+    
+    /**
+     整数反转
+     输入：x = 123
+     输出：321
+     示例 2：
+
+     输入：x = -123
+     输出：-321
+     示例 3：
+
+     输入：x = 120
+     输出：21
+     示例 4：
+
+     输入：x = 0
+     输出：0
+
+     */
+    func reverse(_ x: Int) -> Int {
+        var x = x
+        var res = 0
+        while x != 0 {
+            let t = x % 10
+            res = res * 10 + t
+            x = x / 10
+        }
+        if res > Int32.max || res < Int32.min {
+            return 0
+        }
+        return res
+    }
+    
+    /**
+     给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+
+     s = "leetcode"
+     返回 0
+
+     s = "loveleetcode"
+     返回 2
+     */
+    func firstUniqChar(_ s: String) -> Int {
+        var map: [String : Int] = [:]
+        let strs = s.map { String($0)}
+        for str in strs {
+            if map.keys.contains(str) {
+                let value = map[str]!
+                map[str] = value + 1
+            } else {
+                map[str] = 1
+            }
+        }
+        for (index, str) in strs.enumerated() {
+            if map[str] == 1 {
+                return index
+            }
+        }
+        return -1
+    }
+    
+    /**
+     给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+     注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+
+     示例 1:
+
+     输入: s = "anagram", t = "nagaram"
+     输出: true
+     示例 2:
+
+     输入: s = "rat", t = "car"
+     输出: false
+
+     */
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        var mapS: [String : Int] = [:]
+        var mapT: [String : Int] = [:]
+        guard s.count == t.count else {
+            return false
+        }
+        for str in zip(s, t) {
+            if mapS.keys.contains(String(str.0)) {
+                let value = mapS[String(str.0)]!
+                mapS[String(str.0)] = value + 1
+            } else {
+                mapS[String(str.0)] = 1
+            }
+            if mapT.keys.contains(String(str.1)) {
+                let value = mapT[String(str.1)]!
+                mapT[String(str.1)] = value + 1
+            } else {
+                mapT[String(str.1)] = 1
+            }
+        }
+        for str in s {
+            if mapS[String(str)] != mapT[String(str)] {
+                return false
+            }
+        }
+        return true
+    }
+    
 }
+
+let result = Solution().isAnagram("rat", "car")
+print(result)
+
 
