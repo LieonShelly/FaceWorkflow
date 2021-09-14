64947d2321623c2481ee5756811e2f1ec392ad54
lrj <lirenjun@camera360.com>
2021-09-14 09:28:17 +0800

    update

--- Algri/main.swift
+++ Algri/main.swift
@@ -591,12 +591,195 @@ class Solution {
 //        return set.isEmpty ? 0 : set.first!
     }
     
-    func rotate(_ nums: inout [Int], _ k: Int) {
+    /**给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+     进阶：
 
-      }
+     尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+     你可以使用空间复杂度为 O(1) 的 原地 算法解决这个问题吗？
+      
+
+     示例 1:
+
+     输入: nums = [1,2,3,4,5,6,7], k = 3
+     输出: [5,6,7,1,2,3,4]
+     解释:
+     向右旋转 1 步: [7,1,2,3,4,5,6]
+     向右旋转 2 步: [6,7,1,2,3,4,5]
+     向右旋转 3 步: [5,6,7,1,2,3,4]
+     示例 2:
+
+     输入：nums = [-1,-100,3,99], k = 2
+     输出：[3,99,-1,-100]
+     解释:
+     向右旋转 1 步: [99,-1,-100,3]
+     向右旋转 2 步: [3,99,-1,-100]
+*/
+    func rotate(_ nums: inout [Int], _ k: Int) {
+        let maxIndex = nums.count - 1
+        var newnums = nums
+        for (index, num) in nums.enumerated() {
+            var newIndex = index + k
+            while newIndex > maxIndex {
+                newIndex = newIndex - maxIndex - 1
+                print(newIndex)
+            }
+            newnums[newIndex] = num
+        }
+        nums = newnums
+    }
 }
-var bew = [0,1,0,3,12]
-let result = Solution().singleNumber([2,2,1])
-print(result)
+
+
+class SolutionLinkList {
+    var head: ListNode?
+    
+    func deleteNode(_ node: ListNode?) {
+        // 当前节点的值等于下一个节点的值
+        node?.val = (node?.next!.val)!
+        // 当前节点的next指向下一个节点下一个节点，相当于删除node的next节点
+        node?.next = node?.next?.next
+    }
+    
+    public class ListNode: Equatable {
+        public var val: Int
+        public var next: ListNode?
+        public init(_ val: Int) {
+            self.val = val
+            self.next = nil
+        }
+        
+        static func == (lhs: ListNode, rhs: ListNode) -> Bool {
+            let lhspoint = Unmanaged<AnyObject>.passUnretained(lhs as AnyObject).toOpaque()
+            let lhshashValue = lhspoint.hashValue
+            
+            let rhspoint = Unmanaged<AnyObject>.passUnretained(rhs as AnyObject).toOpaque()
+            let rhshashValue = rhspoint.hashValue
+            return lhshashValue == rhshashValue
+        }
+    }
+   // [1,2,3,4,5]  n = 2
+    func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+        var length = 0
+        var node = head
+        while node != nil {
+            length += 1
+            node = node?.next
+        }
+        let index = length - n
+        if index == 0 {
+            return head?.next
+        }
+        var preNode = head
+        for _ in 0 ..< index - 1 {
+            preNode = preNode?.next
+        }
+        preNode?.next = preNode?.next?.next
+        return head
+    }
+    
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        var current = head
+        var pre: ListNode? = nil
+        while current != nil {
+            let temp = current?.next
+            current?.next = pre
+            pre = current
+            current = temp
+        }
+        return pre
+    }
+    
+    func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        if l1 == nil {
+            return l2
+        }
+        if l2 == nil {
+            return l1
+        }
+        var node1 = l1
+        var node2 = l2
+        let dummy = ListNode(0)
+        var current: ListNode? = dummy
+        while node1 != nil && node2 != nil {
+            // 两个链表小的那个头放到新的链表的尾部，然后继续下一轮的遍历
+            if node1!.val >= node2!.val {
+                current?.next = node2
+                node2 = node2?.next
+            } else {
+                current?.next = node1
+                node1 = node1?.next
+            }
+            current = current?.next
+        }
+        // 把剩余的链表合并到新链表之后
+        current?.next = node2 == nil ? node1 : node2
+        return dummy.next
+    }
+    
+    func isPalindrome(_ head: ListNode?) -> Bool {
+        var index = 0
+        var map: [Int: ListNode] = [:]
+        var node = head
+        while node != nil {
+            map[index] = node
+            node = node?.next
+            index += 1
+        }
+        for (_, key) in map.keys.enumerated() {
+            let firstIndex = key
+            let secondIndex = map.keys.count - 1 - firstIndex
+            if map[firstIndex]!.val != map[secondIndex]!.val {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isPalindrome1(_ head: ListNode?) -> Bool {
+        var fast = head
+        var slow = head
+        while fast != nil, fast?.next != nil {
+            fast = fast?.next?.next
+            slow = slow?.next
+        }
+        // 如果fast不为nil。表明链表的长度为奇数个
+        if fast != nil {
+            slow = slow?.next
+        }
+        fast = head
+        slow = reverseList(slow)
+        while slow != nil {
+            if fast?.val != slow?.val {
+                return false
+            }
+            fast = fast?.next
+            slow = slow?.next
+        }
+        return true
+    }
+}
+
+let head = SolutionLinkList.ListNode(1)
+let node1 = SolutionLinkList.ListNode(2)
+let node2 = SolutionLinkList.ListNode(2)
+let node3 = SolutionLinkList.ListNode(1)
+let node4 = SolutionLinkList.ListNode(5)
+head.next = node1
+node1.next = node2
+node2.next = node3
+//node3.next = node4
+
+let head2 = SolutionLinkList.ListNode(1)
+let node21 = SolutionLinkList.ListNode(2)
+let node22 = SolutionLinkList.ListNode(3)
+let node23 = SolutionLinkList.ListNode(4)
+let node24 = SolutionLinkList.ListNode(5)
+head2.next = node21
+node21.next = node22
+node22.next = node23
+node23.next = node24
+
+let currentHead = SolutionLinkList().isPalindrome(head)
+print(currentHead)
 
 
